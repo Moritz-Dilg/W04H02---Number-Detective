98378e8e42afe091391897f77b3738938ec73aad
Moritz-Dilg <moritz@dilg-online.de>
2024-11-11 14:19:22 +0100

    Game implementation

--- src/pgdp/game/GuessTheNumber.java
+++ src/pgdp/game/GuessTheNumber.java
@@ -4,23 +4,31 @@ import pgdp.InputReader;
 import pgdp.RandomNumberGenerator;
 
 public class GuessTheNumber {
-	private int lives;
+	private int lives = 3;
 	private int points;
 
 	public void guessTheNumber() {
 		System.out.println("Hello, Number Detective!");
-		System.out.println("You have " + lives + " lives and " + points + " points.");
 
 		while (true) {
+			System.out.println("You have " + lives + " lives and " + points + " points.");
 			printMenu();
 			int choice = InputReader.readInt();
+			int randNum = 0;
+			int maxAttempts = 0;
 
 			switch (choice) {
 				case 1:
+					randNum = RandomNumberGenerator.getGenerator().generate(100);
+					maxAttempts = 8;
 					break;
 				case 2:
+					randNum = RandomNumberGenerator.getGenerator().generate(500);
+					maxAttempts = 10;
 					break;
 				case 3:
+					randNum = RandomNumberGenerator.getGenerator().generate(1000);
+					maxAttempts = 10;
 					break;
 				case 4:
 					System.out.println("Goodbye!");
@@ -29,6 +37,54 @@ public class GuessTheNumber {
 				default:
 					System.out.println("This was not a valid choice, please try again.");
 					continue;
+			}
+
+			int attempt;
+			for (attempt = 1; attempt <= maxAttempts; attempt++) {
+				while (attempt == maxAttempts - 1 && points >= 600) {
+					System.out.println("LAST ATTEMPT! Do you want to buy a hint for 600 points? (1) yes (2) no");
+					int choiceBuyHint = InputReader.readInt();
+					if (choiceBuyHint == 1) {
+						points -= 600;
+						System.out.println(randNum % 2 == 0 ? "The number is even!" : "The number is odd!");
+						break;
+					} else if (choiceBuyHint < 1 || choiceBuyHint > 2) {
+						System.out.println("This was not a valid choice, please try again.");
+					}
+				}
+				System.out.println("(" + attempt + "/" + maxAttempts + ") Enter your guess:");
+				int guess = InputReader.readInt();
+				if (guess < randNum) System.out.println("The number is higher.");
+				else if (guess > randNum) System.out.println("The number is lower.");
+				else {
+					System.out.println("Congrats! You guessed the correct number.");
+					switch (choice) {
+						case 1:
+							points += 200;
+							break;
+						case 2:
+							points += 200;
+							lives += 1;
+							break;
+						case 3:
+							points += 500;
+							lives += 3;
+							break;
+					}
+					attempt = -1;
+					break;
+				}
+			}
+
+			if (attempt != -1) {
+				System.out.println("Sorry, you've used all attempts. The correct number was " + randNum + ".");
+				lives--;
+			}
+
+			if (lives == 0) {
+				System.out.println("Game over! You are out of lives.");
+				System.out.println("You are leaving with " + points + " points!");
+				return;
 			}
 		}
 	}
